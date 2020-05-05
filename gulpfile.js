b1579be418498784914f51d5a44eae0bda176d9c
Rufina <rufinka_91@mail.ru>
2020-05-05 14:15:19 +0300

    update gulpfiles.js

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task("css", function () {
 
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
-   .pipe(imagemin([
+    .pipe(imagemin([
       imagemin.optipng({optimmizationLevel: 3}),
       imagemin.mozjpeg({quality: 75, progressive: true}),
       imagemin.svgo()
