fd89fbfaa6657d82c53cb6b13163866d143bfb08
Rufina <rufinka_91@mail.ru>
2020-05-08 17:26:41 +0300

    finalfiles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 "use strict";
 
 var gulp = require("gulp");
+var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
-var less = require("gulp-less");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var posthtml =  require("gulp-posthtml");
+var csso = require("gulp-csso");
+var webp = require("gulp-webp");
 var server = require("browser-sync").create();
+var del = require("del");
+
+
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -14,16 +22,68 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer()//автопрефиксы добавили дл разных браузеров
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))//минификация файла style.css
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))//папка css сейчас будет в build, из source она уходит
     .pipe(server.stream());
 });
 
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml())
+    .pipe(gulp.dest("build"));
+});
+
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimmizationLevel: 3}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.svgo()
+
+    ]))
+
+    .pipe(gulp.dest("source/img"));
+});
+
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+
+gulp.task("clean", function () {
+  return del("build");//чистит папку build
+});
+
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html",//сама написала
+    "source/*.ico",
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+
+});
+
+
+
+
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/", // когда запускаем localhost:3000 теперь смотрти на папку build
     notify: false,
     open: true,
     cors: true,
@@ -31,7 +91,24 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));//заменить на refresh  on("change", server.reload)
 });
 
-gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+
+gulp.task("build", gulp.series(//запускает последовательность задач
+  "clean",//чистит папку build
+  "copy",//копируе все файлы в папку build
+  "css",//создает style.min.css с префиксами
+  "html"
+));
+
+
+gulp.task("build", gulp.series("css", "html"));//build запускает таски  "css" потом  "html"
+
+gulp.task("start", gulp.series("build", "server"));//сперва запустится здача build потом  задача server
